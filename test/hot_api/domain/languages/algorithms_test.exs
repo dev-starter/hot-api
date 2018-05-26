f10741514bf6c88b72b1dd175a68b8cf9fe8337a
Isaac Yung Lopes <isaaczoi@gmail.com>
2018-05-26 20:07:04 -0300

    Removed trash tests'

--- test/hot_api/domain/languages/algorithms_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HotApi.Domain.Languages.AlgorithmsTest do
-  use ExUnit.Case
-  doctest HotApi.Domain.Languages.Algorithms
-
-  test "greets the world" do
-    assert HotApi.soma(1,2) == 3
-  end
-end
--- test/hot_api/domain/languages/characteristics_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HotApi.Domain.Languages.CharacteristicsTest do
-  use ExUnit.Case
-  doctest HotApi.Domain.Languages.Characteristics
-
-  test "greets the world" do
-    assert HotApi.soma(1,2) == 3
-  end
-end
